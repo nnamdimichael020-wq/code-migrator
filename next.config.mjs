35cf16c9bad1b2f3fb12d74b9d51acf179ae1264
nnamdimichael020-wq <nnamdimichael020@gmail.com>
2026-08-13 02:34:41 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
-/** @type {import('next').NextStyle}.Config */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Cache-Control", value: "no-store, max-age=0" }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
